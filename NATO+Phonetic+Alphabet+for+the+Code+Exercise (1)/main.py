a42a55c51baada3d7441915f90fa437f3dd1a6f3
Maryann Mweru <mwerumaryann@gmail.com>
2023-09-21 17:34:28 +0300

    Updating the phonetic generator to accept strings only

--- NATO+Phonetic+Alphabet+for+the+Code+Exercise (1)/main.py
+++ NATO+Phonetic+Alphabet+for+the+Code+Exercise (1)/main.py
@@ -4,12 +4,21 @@
 import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
-#TODO 1. Create a dictionary in this format:
+# TODO 1. Create a dictionary in this format:
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+def gen_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        gen_phonetic()
+    else:
+        print(output_list)
+
+
+gen_phonetic()
